37c8de9a0a2da4592fdab5802db23e0fd4927e9f
島拓海 <>
2025-09-04 12:15:57 +0900

    ボタン作成

---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ActionButton = ({ label, className, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default ActionButton;
